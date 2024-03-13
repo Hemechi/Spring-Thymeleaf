8940f9c2634e12f0bc388797a7590ed6bd9e272a
Hemechi <helen.leang15@gmail.com>
2024-03-13 09:20:38 +0700

    Thymeleaf

--- src/main/java/thymeleaf/model/Task.java
+++ src/main/java/thymeleaf/model/Task.java
@@ -11,21 +11,5 @@ public class Task {
     private Long id;
     private String description;
     // getters and setters
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
 }
 
